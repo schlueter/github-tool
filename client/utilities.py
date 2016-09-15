89986a552bfa9a20b1400bb49f7a60fcccabd28c
Schlueter <bs@schlueter.blue>
2016-09-15 17:57:14 -0400

    Dont require token

--- client/utilities.py
+++ client/utilities.py
@@ -1,19 +1,20 @@
-# -*- mode: python -*-
-# vi: set ft=python :
 import os
 import json
 
 import requests
 
 
-class ResourceNotAvailable(Exception):
+url_prefix = 'https://api.github.com/'
+headers = dict(Accept='application/vnd.github.v3+json')
+
+if 'GITHUB_TOKEN' in os.environ:
+    headers['Authorization'] = "token %s" % os.environ['GITHUB_TOKEN']
+else:
+    # TODO make the api interaction display the api limit blocker
     pass
 
-
-url_prefix = 'https://api.github.com/'
-github_access_token = os.environ['GITHUB_ACCESS_TOKEN']
-headers = dict(Accept='application/vnd.github.v3+json',
-               Authorization="token %s" % github_access_token)
+class ResourceNotAvailable(Exception):
+    pass
 
 def api(url, verb=None, json=None):
     if json and not verb:
@@ -43,9 +44,12 @@ def collect_resource(endpoint):
     return resource
 
 def create_label(repo_name, label_name, color):
-    api(url_prefix + 'repos/' + repo_name + '/labels',
+    return api(url_prefix + 'repos/' + repo_name + '/labels',
         json=dict(name=label_name, color=color))
 
 def copy_labels(source_repo_name, target_repo_name):
-    for label in collect_resource('repos/' + source_repo_name + '/labels'):
-        create_label(target_repo_name, label['name'], label['color'])
+    return [create_label(target_repo_name, label['name'], label['color']) for
+        label in collect_resource('repos/' + source_repo_name + '/labels')]
+
+def get_user_keys(username):
+    return api('users/%s/keys' % username)
