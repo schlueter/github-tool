147e456777c199df50c8c8f083901fb7bbff0f17
Brandon Schlueter <bschlueter@beachbody.com>
2020-05-15 17:48:17 -0700

    Add comments, types

--- lib/github_api_client/utilities.py
+++ lib/github_api_client/utilities.py
@@ -1,5 +1,9 @@
+"""
+Utilities for interacting with GitHub's api
+"""
 import os
 import json as JSON
+from typing import Literal, Optional, Union
 
 import requests
 
@@ -10,10 +14,26 @@ HEADERS = dict(Accept='application/vnd.github.v3+json')
 if 'GITHUB_TOKEN' in os.environ:
     HEADERS['Authorization'] = "token %s" % os.environ['GITHUB_TOKEN']
 
-class ResourceNotAvailable(Exception):
-    pass
+HTTPVerbsType = Optional[Literal['GET', 'POST', 'PUT']]
+JSONType = Optional[Union[str, int, float, bool, dict, list]]
 
-def api(url, verb=None, json=None, headers={}):
+
+class ResourceNotAvailable(Exception):
+    """
+    Exception indicating that the resource is not available
+    """
+
+
+def api(
+        url: str,
+        verb: HTTPVerbsType = None,
+        json: JSONType = None,
+        headers: Optional[dict] = None
+        ):
+    """
+    API call method
+    """
+
     if json and not verb:
         verb = 'POST'
     elif not verb:
@@ -22,13 +42,18 @@ def api(url, verb=None, json=None, headers={}):
     if not url.startswith(URL_PREFIX):
         url = URL_PREFIX + url
 
-    kwargs = dict(headers={**HEADERS, **headers})
+    kwargs = dict(headers={**HEADERS, **(headers or {})})
     if json:
         kwargs['json'] = json
 
     return requests.request(verb, url, **kwargs)
 
-def collect_resource(endpoint):
+
+def collect_resource(endpoint: str):
+    """
+    For resources which may have multiple pages of content, collect all pages
+    into a list.
+    """
     page = api(endpoint)
     if page.status_code > 399:
         raise ResourceNotAvailable(page.status_code)
@@ -40,11 +65,26 @@ def collect_resource(endpoint):
         resource.extend(JSON.loads(page.text))
     return resource
 
-def create_label(repo_name, label_name, color):
+
+def create_label(repo_name: str, label_name: str, color: str):
+    """
+    Shortcut to create a label (for issues) on a repository
+    """
     return api('repos/' + repo_name + '/labels',
                json=dict(name=label_name, color=color))
 
-def update_label(repo_name, current_name, new_name=None, new_color=None, new_description=None):
+
+def update_label(
+        repo_name: str,
+        current_name: str,
+        new_name: Optional[str] = None,
+        new_color: Optional[str] = None,
+        new_description: Optional[str] = None
+        ):
+    """
+    Shortcut to update a label (for issues) on a repository
+    """
+    preview_accept_header = 'application/vnd.github.symmetra-preview+json'
     payload = dict()
     if new_name:
         payload['name'] = new_name
@@ -54,7 +94,11 @@ def update_label(repo_name, current_name, new_name=None, new_color=None, new_des
         payload['description'] = new_description
     return api('repos/' + repo_name + '/labels/' + current_name,
                json=payload,
-               headers=dict(Accept='application/vnd.github.symmetra-preview+json'))
+               headers=dict(Accept=preview_accept_header))
 
-def get_user_keys(username):
+
+def get_user_keys(username: str):
+    """
+    Shortcut to get a user's public keys
+    """
     return api('users/%s/keys' % username)
